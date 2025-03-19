9ef3ea10ffcaf21cc2104b442935323069ce2279
yahiro816 <admin@MngEnvMCAP820421.onmicrosoft.com>
2025-03-20 01:11:35 +0900

    Committing 18 items from workspace 174e37d3-5584-4fca-8a42-af3b38d00ed6

---
+++ p/Notebook&#32;.Notebook/notebook-content.py
@@ -0,0 +1,29 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "0492abcf-19a7-4e2b-9cad-aaff96609edb",
+# META       "default_lakehouse_name": "ooo",
+# META       "default_lakehouse_workspace_id": "174e37d3-5584-4fca-8a42-af3b38d00ed6"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
